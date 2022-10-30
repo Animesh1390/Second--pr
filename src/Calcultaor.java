7272c2bdf126aad6b5def391cb252209dd9cdccd
Animesh Patel <animesh1390@gmail.com>
2022-10-30 14:37:17 +0000

    Second commit

---
+++ src/Calcultaor.java
@@ -0,0 +1,22 @@
+public class Calcultaor {
+
+        static int a =10;
+        static int b=20;
+
+
+
+
+        public static void addition() {
+            System.out.println(a+b);
+
+
+
+        }
+
+        public static void main(String[] args) {
+
+            addition(); // addition method call in  main
+        }
+    }
+
+
